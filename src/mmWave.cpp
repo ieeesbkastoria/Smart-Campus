951bf7fab8078b3b4af391aca4d307951ec3f470
“Panagiotis <panoskava2006@gmail.com>
2025-05-13 18:26:09 +0300

    Added demo code for mmWave sensor

--- src/mmWave.cpp
+++ src/mmWave.cpp
@@ -2,3 +2,28 @@
 
 #include <Arduino.h>
 #include <SoftwareSerial.h>
+
+
+// Definition of the software serial object
+SoftwareSerial mySerial(5, 4);  // RX, TX
+
+void sendHexData(String hexString) {
+  // Convert hex string to bytes
+  int hexStringLength = hexString.length();
+  byte hexBytes[hexStringLength / 2];
+  
+  for (int i = 0; i < hexStringLength; i += 2) {
+    hexBytes[i / 2] = strtoul(hexString.substring(i, i + 2).c_str(), NULL, 16);
+  }
+  
+  // Send bytes through software serial
+  mySerial.write(hexBytes, sizeof(hexBytes));
+}
+
+void readSerialData() {
+  // Read and print data from software serial
+  while (mySerial.available() > 0) {
+    char incomingByte = mySerial.read();
+    Serial.print(incomingByte);
+  }
+}
