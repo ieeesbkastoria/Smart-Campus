d2b2d5e32d95059bd70d3d9cad158d3f8082dc95
panoskava <panoskava2006@gmail.com>
2025-05-22 14:09:36 +0300

    Added 2nd function to implemenansion file

--- include/mmWave.h
+++ include/mmWave.h
@@ -5,6 +5,9 @@
 #define RX2_PIN 19
 #define TX2_PIN 20
 
+// Define range prefix to identify lines from the sensor
+#define RANGE_PREFIX "Range "
+
 #include <Arduino.h>
 
 void sendHexData(String hexString);
